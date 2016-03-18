689b3fb82fc30c235b506b75ad6e36522ecda103
N.Chihara <nchihara@go2group.com>
2016-03-18 18:18:20 +0900

    done:Create a GUI with templates and AUI

--- adminUI/src/main/java/com/atlassian/plugins/tutorial/refapp/MyPluginServlet.java
+++ adminUI/src/main/java/com/atlassian/plugins/tutorial/refapp/MyPluginServlet.java
@@ -2,6 +2,7 @@ package com.atlassian.plugins.tutorial.refapp;
 
 import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.sal.api.user.UserKey;
 import com.atlassian.sal.api.user.UserManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +18,7 @@ import java.io.IOException;
 import java.net.URI;
 import com.atlassian.sal.api.auth.LoginUriProvider;
 import com.atlassian.sal.api.user.UserManager;
+import com.atlassian.templaterenderer.TemplateRenderer;
 import javax.inject.Named;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 
@@ -30,27 +32,31 @@ public class MyPluginServlet extends HttpServlet
     private final UserManager userManager;
     @ComponentImport
     private final LoginUriProvider loginUriProvider;
+    @ComponentImport
+    private final TemplateRenderer  templateRenderer;
 
     @Inject
-    public MyPluginServlet(UserManager userManager, LoginUriProvider loginUriProvider)
+    public MyPluginServlet(UserManager userManager, LoginUriProvider loginUriProvider, TemplateRenderer templateRenderer)
     {
         this.userManager = userManager;
         this.loginUriProvider = loginUriProvider;
+        this.templateRenderer = templateRenderer;
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-        String userName = userManager.getRemoteUsername(req);
-        if( (userName == null) || (!userManager.isSystemAdmin(userName)))
+        // since API 2.10, getRemoteUserName & isSystemAdmin are deprecated
+        UserKey userKey = userManager.getRemoteUserKey(req);
+        if( (userKey == null) || (!userManager.isAdmin(userKey)))
         {
             redirectToLogin(req, resp);
             return;
         }
 
-        resp.setContentType("text/html");
-        resp.getWriter().write("<html><body>Hello! from MyPluginServlet.</body></html>");
-
+        // resp.setContentType("text/html");
+        // resp.getWriter().write("<html><body>Hello! from MyPluginServlet.</body></html>");
+        templateRenderer.render("admin.vm", resp.getWriter());
     }
 
     private void redirectToLogin(HttpServletRequest req, HttpServletResponse resp) throws IOException
